671ee044bd87e8242a38e34ac3297dec341405f1
Amar Prasad <amarprasad77354@gmail.com>
2023-05-03 18:45:00 +0530

    data split

--- data/database.js
+++ data/database.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 // Database Part
 export const connectDB = () => {
-    mongoose.connect("mongodb://127.0.0.1:27017", {
+    mongoose.connect(process.env.MONGO_URI, {
     dbName: "backendapi",
     })
     .then(() => console.log("Database Connected -:)"))
