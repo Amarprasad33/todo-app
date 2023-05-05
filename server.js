db4665c9225180d9c6d745e2126b9f81a16c7bb2
Amar Prasad <amarprasad77354@gmail.com>
2023-05-05 17:55:37 +0530

    custom server

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ connectDB();
 
 
 app.listen(process.env.PORT, () => {
-    console.log(`Server is working on port ${process.env.PORT}`);
+    console.log(`Server is working on port ${process.env.PORT} in ${process.env.NODE_ENV} Mode`);
 })
 
 
