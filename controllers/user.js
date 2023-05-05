2914eb6784f3ba08b777d1d308580695609c7a7a
Amar Prasad <amarprasad77354@gmail.com>
2023-05-05 14:15:24 +0530

    auth with users

--- controllers/user.js
+++ controllers/user.js
@@ -55,6 +55,16 @@ export const getMyProfile = async (req, res) => {
     });
 };
 
+export const logout = (req, res) => {
+    res
+        .status(200)
+        .cookie("token", "", { expires: new Date(Date.now()) })
+        .json({
+            success: true,
+            message: "Logged out",
+            user: req.user,
+        });
+}
 
 
 
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import {  getAllUsers, register, login, getMyProfile  } from "../controllers/user.js";
+import {  getAllUsers, register, login, getMyProfile, logout  } from "../controllers/user.js";
 import { isAuthenticated } from "../middlewares/auth.js";
 
 
@@ -11,6 +11,8 @@ router.get("/all", getAllUsers);
 router.post("/new", register);
 
 router.post("/login", login);
+
+router.get("/logout", logout);
 
 router.get("/me",  isAuthenticated, getMyProfile)
 
